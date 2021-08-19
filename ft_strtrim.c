9eba3360fd5e12f965f71da1e18ae570a8d89907
root <root@DESKTOP-DLGS2TE.localdomain>
2021-08-19 15:38:07 -0300

    correcao

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: daeidi-h <daeidi-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 15:20:50 by daeidi-h          #+#    #+#             */
-/*   Updated: 2021/08/19 14:03:29 by daeidi-h         ###   ########.fr       */
+/*   Updated: 2021/08/19 15:36:34 by daeidi-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	end = ft_strlen(s1);
-	while (end > 0 && ft_strchr(set, s1[end -1]))
+	while (end > 0 && ft_strchr(set, s1[end]))
 		end--;
-	str = ft_substr(s1, start, end - start);
-	if (end - start == 0)
-		str[0] = '\0';
+	str = ft_substr(s1, start, end - start + 1);
 	return (str);
 }
