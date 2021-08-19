5c63879b9976a42f3a3a1ce8ba5d36b7050cecd6
root <root@DESKTOP-DLGS2TE.localdomain>
2021-08-19 20:55:23 -0300

    ok

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: daeidi-h <daeidi-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 15:20:50 by daeidi-h          #+#    #+#             */
-/*   Updated: 2021/08/19 10:44:01 by daeidi-h         ###   ########.fr       */
+/*   Updated: 2021/08/19 20:52:20 by daeidi-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,26 @@
  */
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
+	unsigned int	i;
+
+	if (!dst && !src)
+		return (NULL);
 	if (dst > src)
 	{
-		while (len--)
-			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
+		while (len > 0)
+		{
+			((char *)dst)[len - 1] = ((char *)src)[len - 1];
+			len--;
+		}
 	}
-	if (dst < src)
-		ft_memcpy(dst, src, len);
+	else
+	{
+		i = 0;
+		while (i < len)
+		{
+			((char *)dst)[i] = ((char *)src)[i];
+			i++;
+		}
+	}
 	return (dst);
 }
