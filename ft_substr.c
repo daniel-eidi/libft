48f8958c8044b89563dd87357649fbf961e50bc6
root <root@DESKTOP-DLGS2TE.localdomain>
2021-08-19 16:50:24 -0300

    ok

--- ft_substr.c
+++ ft_substr.c
@@ -6,26 +6,47 @@
 /*   By: daeidi-h <daeidi-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 15:20:50 by daeidi-h          #+#    #+#             */
-/*   Updated: 2021/08/19 12:46:22 by daeidi-h         ###   ########.fr       */
+/*   Updated: 2021/08/19 16:47:59 by daeidi-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "libft.h"#include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, \
 size_t len)
 {
-	char			*new;
-	size_t			siz;
-	char			*ori;
+	char	*new;
+	size_t	siz;
+	char	*ori;
 
+	if (!s)
+		return (NULL);
 	ori = (char *) s;
 	siz = ft_strlen(s);
-	new = ft_calloc(sizeof(char), (len + 1));
+	if (start > siz)
+		return (ft_strdup(""));
+	if (len > siz)
+		len = siz - start;
+	new = malloc(sizeof(char) * (len + 1));
 	if (new == NULL)
 		return (NULL);
-	if (start > siz - 1)
-		return (new);
 	ft_strlcpy(new, ori + start, len + 1);
 	return (new);
 }
+// char	*ft_substr(char const *s, unsigned int start, \
+// size_t len)
+// {
+// 	char			*new;
+// 	size_t			siz;
+// 	char			*ori;
+
+// 	ori = (char *) s;
+// 	siz = ft_strlen(s);
+// 	new = ft_calloc(sizeof(char), (len + 1));
+// 	if (new == NULL)
+// 		return (NULL);
+// 	if (start > siz - 1)
+// 		return (new);
+// 	ft_strlcpy(new, ori + start, len + 1);
+// 	return (new);
+// }
