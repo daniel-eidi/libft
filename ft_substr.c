87eaac8bb3921df0574c088d4586e4d4cb36ef77
root <root@DESKTOP-DLGS2TE.localdomain>
2021-08-20 10:53:15 -0300

    ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: daeidi-h <daeidi-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 15:20:50 by daeidi-h          #+#    #+#             */
-/*   Updated: 2021/08/20 10:44:47 by daeidi-h         ###   ########.fr       */
+/*   Updated: 2021/08/20 10:52:37 by daeidi-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ size_t len)
 	siz = ft_strlen(s);
 	if (len > siz)
 		len = siz;
+	if (start > siz)
+		return (ft_strdup(""));
 	new = ft_calloc(sizeof(char), (len + 1));
 	if (new == NULL)
 		return (NULL);
-	if (start > siz)
-		return (ft_strdup(""));
 	ft_strlcpy(new, ori + start, len + 1);
 	return (new);
 }
