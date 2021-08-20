56f5c3c18509158d80f5b3ba67220a51c12353a2
root <root@DESKTOP-DLGS2TE.localdomain>
2021-08-20 15:16:46 -0300

    ft_strjoin

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: daeidi-h <daeidi-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 15:36:33 by daeidi-h          #+#    #+#             */
-/*   Updated: 2021/08/20 11:21:46 by daeidi-h         ###   ########.fr       */
+/*   Updated: 2021/08/20 15:16:13 by daeidi-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	if (!s || !f)
 		return (NULL);
 	str = (char *)ft_calloc(sizeof(char), ft_strlen(s) + 1);
+	if (!str)
+		return (NULL);
 	while (s[i] != '\0')
 	{
 		str[i] = f(i, s[i]);
