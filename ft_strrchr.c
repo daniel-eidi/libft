cce5b26b39cbde9ba62bff34edc1b4d88c6d26b4
root <root@DESKTOP-DLGS2TE.localdomain>
2021-08-20 10:31:05 -0300

    ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: daeidi-h <daeidi-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 15:20:50 by daeidi-h          #+#    #+#             */
-/*   Updated: 2021/08/09 09:30:02 by daeidi-h         ###   ########.fr       */
+/*   Updated: 2021/08/20 10:30:33 by daeidi-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,18 @@
  */
 char	*ft_strrchr(const char *s, int c)
 {	
-	int		i;
-	size_t	len;
+	int				i;
+	size_t			len;
+	unsigned char	chr;
 
 	len = ft_strlen (s);
 	i = len;
-	if (!c)
+	chr = (unsigned char) c;
+	if (!chr)
 		return ((char *)s + len);
 	while (i >= 0)
 	{
-		if (s[i] == c)
+		if (s[i] == chr)
 			return ((char *)s + i);
 		i--;
 	}
