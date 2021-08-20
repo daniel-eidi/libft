604bd1bfad38416fc701e91d27872dabf2e1734f
root <root@DESKTOP-DLGS2TE.localdomain>
2021-08-20 15:00:10 -0300

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: daeidi-h <daeidi-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 15:40:04 by daeidi-h          #+#    #+#             */
-/*   Updated: 2021/08/20 14:56:25 by daeidi-h         ###   ########.fr       */
+/*   Updated: 2021/08/20 15:00:00 by daeidi-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*mem;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
+		return (malloc(0));
 	mem = malloc (nmemb * size);
 	if (mem == NULL)
 		return (NULL);
